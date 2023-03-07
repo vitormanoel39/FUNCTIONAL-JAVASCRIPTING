ea13f0cb38155303582c6c9d399afbd44938b774
vitormanoel24 <126992944+vitormanoel39@users.noreply.github.com>
2023-03-06 23:33:13 -0300

    Add files via upload

---
+++ basic-call.js
@@ -0,0 +1,6 @@
+function duckCount() {
+    const arrSlice = Array.prototype.slice.call(arguments);
+    const quackCount = arrSlice.reduce((acc, cur) => 'quack' in cur === true ? ++acc : acc, 0)
+    return quackCount
+  }
+  module.exports = duckCount
